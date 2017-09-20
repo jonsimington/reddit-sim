bd4a6e356b9854bd1c0b8d2b92903696cddc57ee
Jon Simington <jon.simington@mst.edu>
2017-09-19 22:08:45 -0500

    Put auth in a func & add func for getting submissions from a subreddit

--- sim.py
+++ sim.py
@@ -7,11 +7,32 @@
 # Created: Tue Sep 19 21:16:45 2017 (-0500)
 import praw
 import requests
-from secret_settings import r_username, r_password
+from secret_settings import r_username, r_password, client_id, client_secret
 
+def init_reddit():
+    r = praw.Reddit(user_agent='reddit-sim',
+                    client_id=client_id,
+                    client_secret=client_secret,
+                    username=r_username,
+                    password=r_password)
 
-r = praw.Reddit(user_agent='reddit-sim')
+    print("Authenticated to reddit as %s" % r_username)
 
-r.login(r_username, r_password)
+    return r
 
-print("Authenticated to reddit as %s" % r_username)
+def get_submissions(subreddit, *args, **kwargs):
+    s = r.subreddit(subreddit)
+
+    max_submissions = 50
+    submissions = []
+
+    if 'limit' in kwargs:
+        max_submissions = kwargs['limit']
+
+    for s in s.hot(limit=max_submissions):
+        submissions.append(s)
+
+    return submissions
+
+if __name__ == "__main__":
+    r = init_reddit()
