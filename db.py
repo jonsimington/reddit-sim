000a81a446677eb13a7a21a9c7022e16b4f1a59e
Jon Simington <jon.simington@mst.edu>
2017-09-20 00:54:02 -0500

    Store submission's comments

--- db.py
+++ db.py
@@ -9,7 +9,10 @@ import couchdb
 from secret_settings import couch_user, couch_pass, couch_host, couch_port
 
 def init_dbs():
-    databases = ['submissions',]
+    databases = [
+        'submissions',
+        'comments',
+    ]
 
     dbs = {}
 
@@ -32,11 +35,11 @@ def init_dbs():
     return dbs
 
 def store_submission(db, submission):
-
     doc = {
         '_id': submission.id,
         'title': submission.title,
-        'submitter': submission.author.name
+        'submitter': submission.author.name,
+        'subreddit': submission.subreddit.display_name
     }
 
     try:
@@ -44,3 +47,21 @@ def store_submission(db, submission):
         print("Created submission %s." % doc['_id'])
     except couchdb.http.ResourceConflict:
         print("Submission %s already exists." % doc['_id'])
+
+def store_comment(db, comment):
+    try:
+        doc = {
+            '_id': comment.id,
+            'words': comment.body,
+            'author': comment.author.name,
+            'submission': comment.submission.id,
+            'subreddit': comment.submission.subreddit.display_name
+        }
+
+        try:
+            db.save(doc)
+            print("Created comment %s." % doc['_id'])
+        except couchdb.http.ResourceConflict:
+            print("Comment %s already exists." % doc['_id'])
+    except:
+        pass
--- sim.py
+++ sim.py
@@ -6,7 +6,8 @@
 # Author: Jon Simington
 # Created: Tue Sep 19 21:16:45 2017 (-0500)
 from reddit_helpers import init_reddit, get_submissions
-from db import init_dbs, store_submission
+from db import init_dbs, store_submission, store_comment
+from praw.models import MoreComments
 
 if __name__ == "__main__":
     r = init_reddit()
@@ -14,8 +15,15 @@ if __name__ == "__main__":
     dbs = init_dbs()
 
     submissions_store = dbs['submissions']
+    comments_store = dbs['comments']
 
     submissions = get_submissions(r, 'all', limit=30)
 
+
     for s in submissions:
         store_submission(submissions_store, s)
+
+        s.comments.replace_more(limit=0)
+
+        for c in s.comments:
+            store_comment(comments_store, c)
