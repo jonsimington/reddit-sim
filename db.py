de7a322e60c1632e31ff5f335b22bd9c32e21211
Jon Simington <jon.simington@mst.edu>
2017-10-04 20:34:11 -0500

    Move submission sync to db.py

--- db.py
+++ db.py
@@ -7,6 +7,7 @@
 # Created: Tue Sep 19 22:25:45 2017 (-0500)
 import couchdb
 from secret_settings import couch_user, couch_pass, couch_host, couch_port
+from reddit_helpers import get_submissions
 import logging
 
 def init_dbs():
@@ -37,18 +38,20 @@ def init_dbs():
     return dbs
 
 def store_submission(db, submission):
-    doc = {
-        '_id': submission.id,
-        'title': submission.title,
-        'submitter': submission.author.name,
-        'subreddit': submission.subreddit.display_name
-    }
-
     try:
+        doc = {
+            '_id': submission.id,
+            'title': submission.title,
+            'submitter': submission.author.name,
+            'subreddit': submission.subreddit.display_name
+        }
+
         db.save(doc)
         logging.debug("Created submission %s." % doc['_id'])
     except couchdb.http.ResourceConflict:
         logging.debug("Submission %s already exists." % doc['_id'])
+    except AttributeError:
+        logging.debug("Error storing submission.  Author fields probably `None`")
 
 def store_comment(db, comment):
     try:
@@ -65,14 +68,14 @@ def store_comment(db, comment):
             logging.debug("Created comment %s." % doc['_id'])
         except couchdb.http.ResourceConflict:
             logging.debug("Comment %s already exists." % doc['_id'])
-    except:
-        pass
+    except AttributeError:
+        logging.debug("Error storing comment.  Author fields probably `None`")
 
 
 def store_redditor(db, redditor):
     try:
         doc = {
-            '_id': redditor.name,
+            '_id': '(%s)' % redditor.name,
             'name': redditor.name
         }
 
@@ -81,5 +84,33 @@ def store_redditor(db, redditor):
             logging.debug("Created redditor %s." % doc['name'])
         except couchdb.http.ResourceConflict:
             logging.debug("Redditor %s already exists." % doc['name'])
-    except:
-        pass
+        except couchdb.http.ServerError:
+            logging.error("CouchDB server error during store_redditor")
+    except AttributeError:
+        logging.debug("Error storing redditor.  Name field probably `None`")
+
+
+def get_and_store_submissions(r, limit):
+    dbs = init_dbs()
+
+    submissions_store = dbs['submissions']
+    comments_store = dbs['comments']
+    redditors_store = dbs['redditors']
+
+    submissions = get_submissions(r, 'all', limit=limit)
+
+
+
+    print('-' * 80)
+    for s in submissions:
+        store_submission(submissions_store, s)
+        store_redditor(redditors_store, s.author)
+
+        s.comments.replace_more(limit=0)
+
+        print('[%s] (%s) %s' % (submissions.index(s) + 1, len(s.comments), s.title))
+        print('-' * 80)
+
+        for c in s.comments:
+            store_comment(comments_store, c)
+            store_redditor(redditors_store, c.author)
--- sim.py
+++ sim.py
@@ -5,30 +5,11 @@
 #
 # Author: Jon Simington
 # Created: Tue Sep 19 21:16:45 2017 (-0500)
-from reddit_helpers import init_reddit, get_submissions
-from db import init_dbs, store_submission, store_comment, store_redditor
+from reddit_helpers import init_reddit
+from db import get_and_store_submissions
 
 if __name__ == "__main__":
     r = init_reddit()
 
-    dbs = init_dbs()
-
-    submissions_store = dbs['submissions']
-    comments_store = dbs['comments']
-    redditors_store = dbs['redditors']
-
-    submissions = get_submissions(r, 'all', limit=30)
-
-    print('-' * 80)
-    for s in submissions:
-        store_submission(submissions_store, s)
-        store_redditor(redditors_store, s.author)
-
-        s.comments.replace_more(limit=0)
-
-        print('(%s) %s' % (len(s.comments), s.title))
-        print('-' * 80)
-
-        for c in s.comments:
-            store_comment(comments_store, c)
-            store_redditor(redditors_store, c.author)
+    # fetch n submissions from reddit, store submissions, comments & redditors in db
+    get_and_store_submissions(r, limit=200)
